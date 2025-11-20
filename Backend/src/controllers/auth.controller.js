568385df690fb1cf29e4bbebfe55f7f806e4b41f
syaminata <syaminata0987@gmail.com>
2025-11-20 11:09:17 +0000

    correction 11000

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -20,7 +20,7 @@ const register = async (req, res) => {
     }
 
     // Création directe, le mot de passe sera hashé automatiquement dans le modèle
-    const user = await User.create({ name, email: email || null, numero, password, role });
+    const user = await User.create({ name, email: email || undefined, numero, password, role });
 
     res.status(201).json({
       message: 'Utilisateur créé avec succès',
