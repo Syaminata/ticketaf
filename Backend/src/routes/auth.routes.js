e646fe756d3e94a85df3c97ba753065b3c829a5f
syaminata <syaminata0987@gmail.com>
2025-12-19 13:24:37 +0000

    searation role

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/auth.controller');
-
+const { loginDriver } = require('../controllers/driver.auth.controller');
 /**
  * @swagger
  * /auth/register:
