fae94c885e1a7261743c94ffa94487ebb9256c26
syaminata <syaminata0987@gmail.com>
2026-01-08 10:59:11 +0000

    supression cascade

--- Backend/src/models/voyage.model.js
+++ Backend/src/models/voyage.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const Reservation = require('./reservation.model');
+const Colis = require('./colis.model');
 
 const voyageSchema = new mongoose.Schema({
   driver: { type: mongoose.Schema.Types.ObjectId, ref: 'Driver', required: true },
@@ -10,4 +12,20 @@ const voyageSchema = new mongoose.Schema({
   availableSeats: { type: Number, required: true, default: 5 } 
 }, { timestamps: true });
 
+voyageSchema.pre('findOneAndDelete', async function (next) {
+  try {
+    const voyage = await this.model.findOne(this.getQuery());
+    if (!voyage) return next();
+
+    // Supprimer les réservations liées à ce voyage
+    await Reservation.deleteMany({ voyage: voyage._id });
+
+    // Supprimer les colis liés à ce voyage
+    await Colis.deleteMany({ voyage: voyage._id });
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = mongoose.model('Voyage', voyageSchema);
